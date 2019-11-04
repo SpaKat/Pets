2c82942b831d6c7ea3e83cd262a3dd8b3d2e9a54
SpaKat <deanlaga@yahoo.com>
2019-11-04 16:07:39 -0500

    the start of graphics

---
+++ Pets/src/localGraphics/PetGUI.java
@@ -0,0 +1,23 @@
+package localGraphics;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PetGUI extends Application {
+
+	
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		
+		
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
