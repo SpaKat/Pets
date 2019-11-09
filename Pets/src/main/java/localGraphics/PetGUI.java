017b5f280b00e3aa1d5959ffe4076c222e35bb67
P0TAT00000 <zachtaylor25q@gmail.com>
2019-11-09 09:01:36 -0500

    Working button to feed Pet.

--- Pets/src/main/java/localGraphics/PetGUI.java
+++ Pets/src/main/java/localGraphics/PetGUI.java
@@ -8,10 +8,13 @@ import engine.Game;
 import engine.User;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import pet.Pet;
+import world.Consumable;
 
 public class PetGUI extends Application {
 
@@ -22,6 +25,8 @@ public class PetGUI extends Application {
 	
 	private Pane root;
 	private Scene scene1;
+	private Label petPrint;
+	private Button feed;
 	private Constants consts;
 	private HashMap<String,Double> c;
 	private Game game;
@@ -33,6 +38,8 @@ public class PetGUI extends Application {
 	public PetGUI() {
 		root = new Pane();
 		scene1 = new Scene(root);
+		petPrint = new Label("");
+		feed = new Button("FEED");
 		try {
 			consts = new Constants();
 		} catch (IOException e) {
@@ -42,21 +49,41 @@ public class PetGUI extends Application {
 		c = new HashMap<String,Double>();
 		c = consts.getCONSTANTS();
 		
+		petPrint.setTranslateX(10);
+		petPrint.setTranslateY(30);
+		
+		feed.setTranslateX(600);
+		feed.setTranslateY(30);
+		
 		game 	= new Game();
 		user1 	= new User();
 		user1.addPetToGame(new Pet("Alfred", c, 500, 500, 500, 500));
 		game.addUser(user1);
 		sprites = new PetSprites(game.getUsers(), c.get(MAXPPU).intValue());
 		spr = sprites.getSprites();
-		
+		int i = 0;
+		int j = 0;
 		for(Circle[] a:spr) {
 			for(Circle b:a) {
 				if(b!=null) {
 					root.getChildren().add(b);
+					///UPDATE TO GET SPECIFIC USER FROM GAME AND USER
+					petPrint.setText(game.getUsers().get(i).getPets().get(j).toString());
 				}
+				j++;
 			}
+			i++;
 		}
 		
+		feed.setOnMouseReleased(e -> {
+			Pet temp = game.getUsers().get(0).getPets().get(0);
+			temp.consume(new Consumable("Apple", 2, 2, 2, 2, 2, false));
+			temp.hungerLU(200);
+			petPrint.setText(temp.toString());
+		});
+		
+		root.getChildren().add(petPrint);
+		root.getChildren().add(feed);
 		
 	}
 	
